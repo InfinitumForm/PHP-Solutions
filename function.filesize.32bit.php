924884fe07700cd03a8786b1cbab0da52cb0b1ee
Ivijan-Stefan Stipic <creativform@gmail.com>
2018-01-19 22:44:48 +0100

    Get real file size on 32bit operating system or 32bit PHP
    This function can get real file size from files larger than 2GB precisely.

---
+++ function.filesize.32bit.php
@@ -0,0 +1,73 @@
+<?php
+
+/**
+ * Get real file size on 32bit operating system or 32bit PHP
+ * @author Ivijan-Stefan Stipic <creativform@gmail.com>
+ * @version 1.0.0 BETA
+ *
+ * @require /class.os.php
+ * @return string numeric value of the file size
+**/
+function get_filesize($filename)
+{
+  $buffer = '';
+  $cnt    = 0;
+  $chunk_size = 1024*1024;
+
+  $filename = dirname(__FILE__).'/'.$filename;
+  $filename = str_replace("\\","/",$filename);
+
+  // First try get via SHELL if is active
+  if(OS::is_win())
+  {
+    if(function_exists('shell_exec'))
+    {
+      $size = shell_exec('for %F in ("'.$filename.'") do @echo %~zF');
+      if(!empty($size))
+      {
+        $cnt = trim($size);
+      }
+    }
+  }
+  else
+  {
+    if(function_exists('shell_exec'))
+    {
+      $size = shell_exec('stat -c %s '.$filename);
+      if(!empty($size))
+      {
+        $cnt = trim($size);
+      }
+    }
+  }
+
+  // SHELL fail in most cases, do PHP magic
+  if($cnt === 0)
+  {
+    if(OS::is_php64()!==false && OS::is_os64()!==false)
+    {
+      // 64 bit
+      $cnt = @filesize($filename);
+    }
+    else
+    {
+      // 32 bit
+      $handle = fopen($filename, 'rb');
+
+      if ($handle === false) {
+        return false;
+      }
+
+      while (!feof($handle)) {
+        $buffer = fread($handle, $chunk_size);
+        if ($buffer!==false) {
+          $cnt += strlen($buffer);
+        }
+      }
+
+      fclose($handle);
+    }
+  }
+
+  return $cnt;
+}
