648339ff14ca3cd65b955efbff11957fd11f80bd
Ivijan-Stefan Stipic <creativform@gmail.com>
2018-01-21 04:54:10 +0100

    Update function.filesize.32bit.php

--- function.filesize.32bit.php
+++ function.filesize.32bit.php
@@ -13,61 +13,60 @@ function get_filesize($filename)
   $buffer = '';
   $cnt    = 0;
   $chunk_size = 1024*1024;
+  $filename = str_replace(array("\\",'//'),"/",$filename);
 
-  $filename = dirname(__FILE__).'/'.$filename;
-  $filename = str_replace("\\","/",$filename);
-
-  // First try get via SHELL if is active
-  if(OS::is_win())
-  {
-    if(function_exists('shell_exec'))
+  if(file_exists($filename)) :
+    // First try get via SHELL if is active
+    if(OS::is_win())
     {
-      $size = shell_exec('for %F in ("'.$filename.'") do @echo %~zF');
-      if(!empty($size))
+      if(function_exists('shell_exec'))
       {
-        $cnt = trim($size);
+        $size = shell_exec('for %F in ("'.$filename.'") do @echo %~zF');
+        if(!empty($size))
+        {
+          $cnt = trim($size);
+        }
       }
-    }
-  }
-  else
-  {
-    if(function_exists('shell_exec'))
-    {
-      $size = shell_exec('stat -c %s '.$filename);
-      if(!empty($size))
-      {
-        $cnt = trim($size);
-      }
-    }
-  }
-
-  // SHELL fail in most cases, do PHP magic
-  if($cnt === 0)
-  {
-    if(OS::is_php64()!==false && OS::is_os64()!==false)
-    {
-      // 64 bit
-      $cnt = @filesize($filename);
     }
     else
     {
-      // 32 bit
-      $handle = fopen($filename, 'rb');
-
-      if ($handle === false) {
-        return false;
-      }
-
-      while (!feof($handle)) {
-        $buffer = fread($handle, $chunk_size);
-        if ($buffer!==false) {
-          $cnt += strlen($buffer);
+      if(function_exists('shell_exec'))
+      {
+        $size = shell_exec('stat -c %s '.$filename);
+        if(!empty($size))
+        {
+          $cnt = trim($size);
         }
       }
-
-      fclose($handle);
     }
-  }
 
+    // SHELL fail in most cases, do PHP magic
+    if($cnt === 0)
+    {
+      if(OS::is_php64()!==false && OS::is_os64()!==false)
+      {
+        // 64 bit
+        $cnt = @filesize($filename);
+      }
+      else
+      {
+        // 32 bit
+        $handle = fopen($filename, 'rb');
+
+        if ($handle === false) {
+          return false;
+        }
+
+        while (!feof($handle)) {
+          $buffer = fread($handle, $chunk_size);
+          if ($buffer!==false) {
+            $cnt += strlen($buffer);
+          }
+        }
+
+        fclose($handle);
+      }
+    }
+  endif;
   return $cnt;
 }
