4d5e3b8615ce8aabd4ebf2ee8923ad8036fe56a8
CreativForm <creativform@gmail.com>
2021-02-25 18:33:48 +0100

    Improved function.filesize.32bit.php

--- function.filesize.32bit.php
+++ function.filesize.32bit.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Get real file size on 32bit operating system or 32bit PHP
  * @author Ivijan-Stefan Stipic <creativform@gmail.com>
@@ -7,66 +6,65 @@
  *
  * @require /class.os.php
  * @return string numeric value of the file size
-**/
+ **/
 function get_filesize($filename)
 {
-  $buffer = '';
-  $cnt    = 0;
-  $chunk_size = 1024*1024;
-  $filename = str_replace(array("\\",'//'),"/",$filename);
+    $buffer     = '';
+    $cnt        = 0;
+    $chunk_size = 1024 * 1024;
+    $filename   = str_replace(array(
+        "\\",
+        '//'
+    ), "/", $filename);
 
-  if(file_exists($filename)) :
-    // First try get via SHELL if is active
-    if(OS::is_win())
-    {
-      if(function_exists('shell_exec'))
-      {
-        $size = shell_exec('for %F in ("'.$filename.'") do @echo %~zF');
-        if(!empty($size))
-        {
-          $cnt = trim($size);
-        }
-      }
-    }
-    else
-    {
-      if(function_exists('shell_exec'))
-      {
-        $size = shell_exec('stat -c %s '.$filename);
-        if(!empty($size))
-        {
-          $cnt = trim($size);
-        }
-      }
-    }
+    if (file_exists($filename)):
 
-    // SHELL fail in most cases, do PHP magic
-    if($cnt === 0)
-    {
-      if(OS::is_php64()!==false && OS::is_os64()!==false)
-      {
-        // 64 bit
-        $cnt = @filesize($filename);
-      }
-      else
-      {
-        // 32 bit
-        $handle = fopen($filename, 'rb');
+		if(class_exists('COM')) {
+			$fso = new COM("Scripting.FileSystemObject");
+			return $fso->GetFile($filename)->Size;
+		}
 
-        if ($handle === false) {
-          return false;
+		// First try get via SHELL if is active
+        if (OS::is_win()) {
+            if (function_exists('shell_exec')) {
+                $size = shell_exec('for %F in ("' . $filename . '") do @echo %~zF');
+                if (!empty($size)) {
+                    $cnt = trim($size);
+                }
+            }
+        } else {
+            if (function_exists('shell_exec')) {
+                $size = shell_exec('stat -c %s ' . $filename);
+                if (!empty($size)) {
+                    $cnt = trim($size);
+                }
+            }
         }
 
-        while (!feof($handle)) {
-          $buffer = fread($handle, $chunk_size);
-          if ($buffer!==false) {
-            $cnt += strlen($buffer);
-          }
+        // SHELL fail in most cases, do PHP magic
+        if ($cnt === 0) {
+            if (OS::is_php64() !== false && OS::is_os64() !== false) {
+                // 64 bit
+                $cnt = @filesize($filename);
+            } else {
+                // 32 bit
+                $handle = fopen($filename, 'rb');
+                
+                if ($handle === false) {
+                    return false;
+                }
+                
+                while (!feof($handle)) {
+                    $buffer = fread($handle, $chunk_size);
+                    if ($buffer !== false) {
+                        $cnt += strlen($buffer);
+                    }
+                }
+                
+                fclose($handle);
+            }
         }
 
-        fclose($handle);
-      }
-    }
-  endif;
-  return $cnt;
+    endif;
+    return $cnt;
 }
